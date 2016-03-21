eb17ab73b2aa3886fd9d771543850303fb4df835
Scott <sfrancis@misinteractive.com>
2016-03-21 13:38:41 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+
+
+module.exports={
+    Notify:require('./lib/notify'),
+    $Notify:require('./lib/notifyProvider'),
+    Dialog:require('./lib/dialog'),
+    $Dialog:require('./lib/dialogProvider')
+};
